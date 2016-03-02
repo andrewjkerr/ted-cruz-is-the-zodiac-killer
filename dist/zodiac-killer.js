d60811fb088445ab2827afb7cc829a79488ceef4
Andrew Kerr <me+github@andrewjkerr.com>
2016-03-02 15:23:26 -0500

    ... huge bugfix. The DOM is weird.

--- dist/zodiac-killer.js
+++ dist/zodiac-killer.js
@@ -1,1 +1,19 @@
-document.body.innerHTML = document.body.innerHTML.replace(/ted cruz/gi, 'The Zodiac Killer');
+/*
+  Huge thanks to Brock Adams on Stack Overflow for his answer here: https://stackoverflow.com/questions/14046938/globally-replacing-text-on-a-page-breaks-garbles-it
+*/
+
+const TEXT_NODE       = 3;
+const ELEMENT_NODE    = 1;
+
+replaceTextValues(document.body, /ted cruz/gi, "The Zodiac Killer");
+
+function replaceTextValues (node, regex, replaceWith) {
+    if (node.nodeType === TEXT_NODE) {
+        node.nodeValue  = node.nodeValue.replace (regex, replaceWith);
+    }
+    else if (node.nodeType === ELEMENT_NODE) {
+        for (var K = 0, numNodes = node.childNodes.length;  K < numNodes;  ++K) {
+            replaceTextValues (node.childNodes[K], regex, replaceWith);
+        }
+    }
+}
